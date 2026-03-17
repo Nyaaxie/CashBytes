692029c7b8edfcf5c3cffd41c96f81e700636a31
James Brizuela <jamesbrizuela513@gmail.com>
2026-03-17 09:58:42 +0800

    testing changes

--- resources/views/savings/create.blade.php
+++ resources/views/savings/create.blade.php
@@ -93,6 +93,7 @@ function toggleCustomCategory(value) {
     const group = document.getElementById('custom-category-group');
     const input = document.getElementById('custom-category-input');
 
+
     if (value === 'Custom') {
         group.style.display = 'block';
         input.required = true;
